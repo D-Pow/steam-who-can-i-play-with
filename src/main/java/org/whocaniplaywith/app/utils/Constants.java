63bfeddbf5fea1cf92a06eee4f32a7845e034a62
D-Pow <djp460@nyu.edu>
2021-03-07 14:24:24 -0700

    Add new Steam API URL for getting all games Steam provides

--- src/main/java/org/whocaniplaywith/app/utils/Constants.java
+++ src/main/java/org/whocaniplaywith/app/utils/Constants.java
@@ -7,4 +7,5 @@ public class Constants {
     public static String URL_STEAM_GET_OWNED_GAMES = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/";
     public static String URL_STEAM_GET_GAME_DETAILS = "https://store.steampowered.com/api/appdetails";
     public static String URL_STEAM_GET_GAME_STATS_FOR_USER = "http://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/";
+    public static String URL_STEAM_GET_ALL_GAME_IDS_AND_NAMES = "https://api.steampowered.com/ISteamApps/GetAppList/v2/";
 }

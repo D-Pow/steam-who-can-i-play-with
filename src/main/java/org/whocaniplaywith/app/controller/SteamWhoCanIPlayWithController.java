826f3cd7d94ac6da39ee024faa65ad4234234972
D-Pow <djp460@nyu.edu>
2021-03-07 18:10:20 -0700

    Add logic to get all friend's multiplayer games
    TODO: Move the different logic to separate functions, and make them accessible through new endpoints

--- src/main/java/org/whocaniplaywith/app/controller/SteamWhoCanIPlayWithController.java
+++ src/main/java/org/whocaniplaywith/app/controller/SteamWhoCanIPlayWithController.java
@@ -12,6 +12,7 @@ import org.whocaniplaywith.app.model.SteamOwnedGame;
 import org.whocaniplaywith.app.model.SteamUserProfile;
 import org.whocaniplaywith.app.service.SteamService;
 import org.whocaniplaywith.app.utils.ObjectUtils;
+import org.whocaniplaywith.app.utils.Pair;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -45,6 +46,37 @@ public class SteamWhoCanIPlayWithController {
             List<SteamGameDetails> ownedMultiplayerGames = steamService.getMultiplayerGames(ownedGameDetails);
 
             List<String> friendsIds = steamService.getSteamFriendsIds(userProfile.getSteamid()).get();
+            List<Pair<String, CompletableFuture<List<SteamOwnedGame>>>> friendsOwnedGamesFutures = friendsIds.stream()
+                .map(friendId -> new Pair<>(friendId, steamService.getSteamOwnedGamesForUser(friendId)))
+                .collect(Collectors.toList());
+            List<Pair<String, List<SteamOwnedGame>>> friendsOwnedGames = friendsOwnedGamesFutures.stream()
+                .map(futurePair -> new Pair<>(
+                    futurePair.getKey(),
+                    ObjectUtils.getAllCompletableFutureResults(futurePair.getValue())
+                ))
+                .collect(Collectors.toList());
+            List<Pair<String, List<CompletableFuture<SteamGameDetails>>>> friendsGameDetailsFutures = friendsOwnedGames.stream()
+                .map(pair -> new Pair<>(
+                    pair.getKey(),
+                    pair.getValue().stream()
+                        .map(game -> steamService.getGameDetails(String.valueOf(game.getAppid())))
+                        .collect(Collectors.toList())
+                ))
+                .collect(Collectors.toList());
+            List<Pair<String, List<SteamGameDetails>>> friendsGameDetails = friendsGameDetailsFutures.stream()
+                .map(futurePair -> new Pair<>(
+                    futurePair.getKey(),
+                    ObjectUtils.getAllCompletableFutureResults(futurePair.getValue()).stream()
+                        .filter(game -> game != null)
+                        .collect(Collectors.toList())
+                ))
+                .collect(Collectors.toList());
+            List<Pair<String, List<SteamGameDetails>>> friendsMultiplayerGames = friendsGameDetails.stream()
+                .map(pair -> new Pair<>(
+                    pair.getKey(),
+                    steamService.getMultiplayerGames(pair.getValue())
+                ))
+                .collect(Collectors.toList());
         } catch (InterruptedException | ExecutionException e) {
             log.error("Could not get Steam UserId future. Error = {}", e.getMessage());
 

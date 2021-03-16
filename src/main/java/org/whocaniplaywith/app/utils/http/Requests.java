9f5a176bec6d69a857ceb4c50dc5fac81e5ec2cd
D-Pow <djp460@nyu.edu>
2021-03-15 22:30:59 -0600

    Minor: Remove unused import

--- src/main/java/org/whocaniplaywith/app/utils/http/Requests.java
+++ src/main/java/org/whocaniplaywith/app/utils/http/Requests.java
@@ -21,7 +21,6 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URI;
-import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;

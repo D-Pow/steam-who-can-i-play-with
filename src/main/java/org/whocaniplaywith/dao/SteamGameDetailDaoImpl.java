943351f273ee6bfd28586729505d9f3e30b37aa4
D-Pow <djp460@nyu.edu>
2021-03-24 20:02:28 -0600

    Implement DB caching system since SQLite doesn't allow concurrency
    DB reads will go to cache first, and writes will only proceed if the steamAppId doesn't already exist in the cache

--- src/main/java/org/whocaniplaywith/dao/SteamGameDetailDaoImpl.java
+++ src/main/java/org/whocaniplaywith/dao/SteamGameDetailDaoImpl.java
@@ -8,24 +8,30 @@ import org.whocaniplaywith.app.model.SteamGameDetailsResponse;
 import org.whocaniplaywith.app.utils.Pair;
 import org.whocaniplaywith.dao.persistence.SteamGameDetailRepository;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Repository("SteamGameDetailDao")
 public class SteamGameDetailDaoImpl implements SteamGameDetailDao {
-    @Autowired
     SteamGameDetailRepository steamGameDetailRepository;
+    Map<String, SteamGameDetails> steamGameDetailsKeyValMap= new HashMap<>();
 
-    public SteamGameDetails getSteamGameDetails(String appId) {
-        GameDetail gameDetails = steamGameDetailRepository.getSteamGameDetailsById(appId);
+    public SteamGameDetailDaoImpl(@Autowired SteamGameDetailRepository steamGameDetailRepository) {
+        this.steamGameDetailRepository = steamGameDetailRepository;
 
-        if (gameDetails == null) {
-            return null;
-        }
+        steamGameDetailRepository.findAll().forEach(gameDetail -> {
+            SteamGameDetails steamGameDetails = getSteamGameDetailsFromAppIdAndJsonStinrg(gameDetail);
 
+            steamGameDetailsKeyValMap.put(steamGameDetails.getSteamAppid(), steamGameDetails);
+        });
+    }
+
+    private SteamGameDetails getSteamGameDetailsFromAppIdAndJsonStinrg(GameDetail gameDetail) {
         SteamGameDetailsResponse.GameDetailsResponse steamGameDetailsJson = SteamGameDetailsResponse
-            .getAppIdDetailsMap(gameDetails.getGameDetailJson())
-            .getOrDefault(gameDetails.getSteamAppid(), null);
+            .getAppIdDetailsMap(gameDetail.getGameDetailJson())
+            .getOrDefault(gameDetail.getSteamAppid(), null);
 
         if (steamGameDetailsJson == null) {
             return null;
@@ -34,18 +40,61 @@ public class SteamGameDetailDaoImpl implements SteamGameDetailDao {
         return steamGameDetailsJson.getData();
     }
 
+    private SteamGameDetails getSteamGameDetailsFromAppIdAndJsonStinrg(String appId, String gameDetailsJson) {
+        SteamGameDetailsResponse.GameDetailsResponse steamGameDetailsJson = SteamGameDetailsResponse
+            .getAppIdDetailsMap(gameDetailsJson)
+            .getOrDefault(appId, null);
+
+        if (steamGameDetailsJson == null) {
+            return null;
+        }
+
+        return steamGameDetailsJson.getData();
+    }
+
+    public SteamGameDetails getSteamGameDetails(String appId) {
+        if (steamGameDetailsKeyValMap.containsKey(appId)) {
+            return steamGameDetailsKeyValMap.get(appId);
+        }
+
+        GameDetail gameDetails = steamGameDetailRepository.getSteamGameDetailsById(appId);
+
+        if (gameDetails == null) {
+            return null;
+        }
+
+        return getSteamGameDetailsFromAppIdAndJsonStinrg(gameDetails);
+    }
+
     public boolean saveSteamGameDetails(String appId, String gameDetailJson) {
-        steamGameDetailRepository.save(new GameDetail(appId, gameDetailJson));
+        if (!steamGameDetailsKeyValMap.containsKey(appId)) {
+            steamGameDetailsKeyValMap.put(
+                appId,
+                getSteamGameDetailsFromAppIdAndJsonStinrg(appId, gameDetailJson)
+            );
+
+            steamGameDetailRepository.save(new GameDetail(appId, gameDetailJson));
+        }
 
         return true;
     }
 
     public boolean saveAllSteamGameDetails(List<Pair<String, String>> allGameDetails) {
-        List<GameDetail> gameDetails = allGameDetails.stream()
-            .map(gameDetailPair -> new GameDetail(gameDetailPair.getKey(), gameDetailPair.getValue()))
+        List<Pair<String, String>> unsavedGameDetails = allGameDetails.stream()
+            .filter(gameDetailPair -> !steamGameDetailsKeyValMap.containsKey(gameDetailPair.getKey()))
             .collect(Collectors.toList());
 
-        steamGameDetailRepository.saveAll(gameDetails);
+        if (!unsavedGameDetails.isEmpty()) {
+            List<GameDetail> gameDetails = unsavedGameDetails.stream()
+                .peek(gameDetailPair -> steamGameDetailsKeyValMap.put(
+                    gameDetailPair.getKey(),
+                    getSteamGameDetailsFromAppIdAndJsonStinrg(gameDetailPair.getKey(), gameDetailPair.getValue())
+                ))
+                .map(gameDetailPair -> new GameDetail(gameDetailPair.getKey(), gameDetailPair.getValue()))
+                .collect(Collectors.toList());
+
+            steamGameDetailRepository.saveAll(gameDetails);
+        }
 
         return true;
     }
